426b8560a60dfc295597c6e5ad460f9fabfa5ddb
Shock9616 <Shock9616@users.noreply.github.com>
2023-01-04 14:24:20 -0800

    Added minimum window size

--- Phoenix/Phoenix/PhoenixApp.swift
+++ Phoenix/Phoenix/PhoenixApp.swift
@@ -12,6 +12,8 @@ struct PhoenixApp: App {
   var body: some Scene {
     WindowGroup {
       ContentView()
+            .frame(minWidth: 800, idealWidth: 1900, maxWidth: .infinity,
+                   minHeight: 445, idealHeight: 1080, maxHeight: .infinity)
     }
   }
 }
