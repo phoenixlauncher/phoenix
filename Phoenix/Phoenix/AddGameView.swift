d0747bfa1129abe28bf26cde52d4fc4ad14af6d6
Shock9616 <Shock9616@users.noreply.github.com>
2023-08-22 21:27:41 -0700

    Added help buttons to add/edit game views

--- Phoenix/Phoenix/AddGameView.swift
+++ Phoenix/Phoenix/AddGameView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct AddGameView: View {
     @Environment(\.dismiss) private var dismiss
+    @Environment(\.openURL) private var openURL
 
     @State private var nameInput: String = ""
     @State private var iconInput: String = ""
@@ -31,97 +32,98 @@ struct AddGameView: View {
         ScrollView {
             VStack(alignment: .leading) {
                 Group {
-                HStack {
-                    Text("Name")
-                        .frame(width: 70, alignment: .leading)
-                    TextField("Enter game name", text: $nameInput)
-                        .padding()
-                        .accessibility(label: Text("NameInput"))
-                }
+                    HStack {
+                        Text("Name")
+                            .frame(width: 70, alignment: .leading)
+                        TextField("Enter game name", text: $nameInput)
+                            .padding()
+                            .accessibility(label: Text("NameInput"))
+                    }
 
-                HStack {
-                    Text("Icon")
-                        .frame(width: 70, alignment: .leading)
-                        .offset(x: -15)
-                    Button(
-                        action: {
-                            iconIsImporting = true
+                    HStack {
+                        Text("Icon")
+                            .frame(width: 70, alignment: .leading)
+                            .offset(x: -15)
+                        Button(
+                            action: {
+                                iconIsImporting = true
 
-                        },
-                        label: {
-                            Text("Browse")
-                        })
-                    Text(iconInput)
-                }
-                .padding()
-                .fileImporter(
-                    isPresented: $iconIsImporting,
-                    allowedContentTypes: [.image],
-                    allowsMultipleSelection: false
-                ) { result in
-                    do {
-                        let selectedFile: URL = try result.get().first ?? URL(fileURLWithPath: "")
-                        iconInput = selectedFile.relativeString
-                        
-                        let iconData = try Data(contentsOf: selectedFile)
-                        
-                        let fileManager = FileManager.default
-                        guard let appSupportURL = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask).first else {
-                            fatalError("Unable to retrieve application support directory URL")
-                        }
-                        
-                        let cachedImagesDirectoryPath = appSupportURL.appendingPathComponent("Phoenix/cachedImages", isDirectory: true)
-                        
-                        if !fileManager.fileExists(atPath: cachedImagesDirectoryPath.path) {
+                            },
+                            label: {
+                                Text("Browse")
+                            }
+                        )
+                        Text(iconInput)
+                    }
+                    .padding()
+                    .fileImporter(
+                        isPresented: $iconIsImporting,
+                        allowedContentTypes: [.image],
+                        allowsMultipleSelection: false
+                    ) { result in
+                        do {
+                            let selectedFile: URL = try result.get().first ?? URL(fileURLWithPath: "")
+                            iconInput = selectedFile.relativeString
+
+                            let iconData = try Data(contentsOf: selectedFile)
+
+                            let fileManager = FileManager.default
+                            guard let appSupportURL = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask).first else {
+                                fatalError("Unable to retrieve application support directory URL")
+                            }
+
+                            let cachedImagesDirectoryPath = appSupportURL.appendingPathComponent("Phoenix/cachedImages", isDirectory: true)
+
+                            if !fileManager.fileExists(atPath: cachedImagesDirectoryPath.path) {
+                                do {
+                                    try fileManager.createDirectory(at: cachedImagesDirectoryPath, withIntermediateDirectories: true, attributes: nil)
+                                    print("Created 'Phoenix/cachedImages' directory")
+                                } catch {
+                                    fatalError("Failed to create 'Phoenix/cachedImages' directory: \(error.localizedDescription)")
+                                }
+                            }
+
+                            var destinationURL: URL
+
+                            if selectedFile.pathExtension.lowercased() == "jpg" || selectedFile.pathExtension.lowercased() == "jpeg" {
+                                destinationURL = cachedImagesDirectoryPath.appendingPathComponent("\(nameInput)icon.jpg")
+                            } else {
+                                destinationURL = cachedImagesDirectoryPath.appendingPathComponent("\(nameInput)icon.png")
+                            }
+
                             do {
-                                try fileManager.createDirectory(at: cachedImagesDirectoryPath, withIntermediateDirectories: true, attributes: nil)
-                                print("Created 'Phoenix/cachedImages' directory")
+                                try iconData.write(to: destinationURL)
+                                iconOutput = destinationURL.relativeString
+                                print("Saved image to: \(destinationURL.path)")
                             } catch {
-                                fatalError("Failed to create 'Phoenix/cachedImages' directory: \(error.localizedDescription)")
+                                print("Failed to save image: \(error.localizedDescription)")
+                            }
+                        } catch {
+                            // Handle failure.
+                            print("Unable to write to file")
+                            print(error.localizedDescription)
+                        }
+                    }
+
+                    HStack {
+                        Text("Platform")
+                            .frame(width: 70, alignment: .leading)
+                        Picker("", selection: $platInput) {
+                            ForEach(Platform.allCases) { platform in
+                                Text(platform.displayName)
                             }
                         }
-                        
-                        var destinationURL: URL
-                        
-                        if selectedFile.pathExtension.lowercased() == "jpg" || selectedFile.pathExtension.lowercased() == "jpeg" {
-                            destinationURL = cachedImagesDirectoryPath.appendingPathComponent("\(nameInput)icon.jpg")
-                        } else {
-                            destinationURL = cachedImagesDirectoryPath.appendingPathComponent("\(nameInput)icon.png")
-                        }
-                        
-                        do {
-                            try iconData.write(to: destinationURL)
-                            iconOutput = destinationURL.relativeString
-                            print("Saved image to: \(destinationURL.path)")
-                        } catch {
-                            print("Failed to save image: \(error.localizedDescription)")
-                        }
-                    } catch {
-                        // Handle failure.
-                        print("Unable to write to file")
-                        print(error.localizedDescription)
-                    }
-                }
-
-                HStack {
-                    Text("Platform")
-                        .frame(width: 70, alignment: .leading)
-                    Picker("", selection: $platInput) {
-                        ForEach(Platform.allCases) { platform in
-                            Text(platform.displayName)
-                        }
-                    }
-                    .labelsHidden()
-                    .padding()
-                }
-
-                HStack {
-                    Text("Command")
-                        .frame(width: 70, alignment: .leading)
-                    TextField("Enter terminal command to launch game", text: $cmdInput)
+                        .labelsHidden()
                         .padding()
-                        .accessibility(label: Text("NameInput"))
-                }
+                    }
+
+                    HStack {
+                        Text("Command")
+                            .frame(width: 70, alignment: .leading)
+                        TextField("Enter terminal command to launch game", text: $cmdInput)
+                            .padding()
+                            .accessibility(label: Text("NameInput"))
+                    }
 
                     HStack {
                         Text("Description")
@@ -152,11 +154,12 @@ struct AddGameView: View {
                         Button(
                             action: {
                                 headIsImporting = true
-                                
+
                             },
                             label: {
                                 Text("Browse")
-                            })
+                            }
+                        )
                         Text(headInput)
                     }
                     .padding()
@@ -168,16 +171,16 @@ struct AddGameView: View {
                         do {
                             let selectedFile: URL = try result.get().first ?? URL(fileURLWithPath: "")
                             headInput = selectedFile.relativeString
-                            
+
                             let headerData = try Data(contentsOf: selectedFile)
-                            
+
                             let fileManager = FileManager.default
                             guard let appSupportURL = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask).first else {
                                 fatalError("Unable to retrieve application support directory URL")
                             }
-                            
+
                             let cachedImagesDirectoryPath = appSupportURL.appendingPathComponent("Phoenix/cachedImages", isDirectory: true)
-                            
+
                             if !fileManager.fileExists(atPath: cachedImagesDirectoryPath.path) {
                                 do {
                                     try fileManager.createDirectory(at: cachedImagesDirectoryPath, withIntermediateDirectories: true, attributes: nil)
@@ -186,15 +189,15 @@ struct AddGameView: View {
                                     fatalError("Failed to create 'Phoenix/cachedImages' directory: \(error.localizedDescription)")
                                 }
                             }
-                            
+
                             var destinationURL: URL
-                            
+
                             if selectedFile.pathExtension.lowercased() == "jpg" || selectedFile.pathExtension.lowercased() == "jpeg" {
                                 destinationURL = cachedImagesDirectoryPath.appendingPathComponent("\(nameInput)_header.jpg")
                             } else {
                                 destinationURL = cachedImagesDirectoryPath.appendingPathComponent("\(nameInput)_header.png")
                             }
-                            
+
                             do {
                                 try headerData.write(to: destinationURL)
                                 headOutput = destinationURL.relativeString
@@ -214,7 +217,6 @@ struct AddGameView: View {
                         TextField("X / 10", text: $rateInput)
                             .padding()
                             .accessibility(label: Text("RatingInput"))
-                        
                     }
                     HStack {
                         Text("Developer")
@@ -222,7 +224,6 @@ struct AddGameView: View {
                         TextField("Enter game developer", text: $devInput)
                             .padding()
                             .accessibility(label: Text("devInput"))
-                        
                     }
                     HStack {
                         Text("Publisher")
@@ -241,58 +242,87 @@ struct AddGameView: View {
             }
             .padding()
 
-            Button(
-                action: {
-                    let dateFormatter: DateFormatter = {
-                        let formatter = DateFormatter()
-                        formatter.dateStyle = .long
-                        return formatter
-                    }()
+            HStack {
+                Spacer().frame(maxWidth: .infinity)
 
-                    let newGame: Game = .init(
-                        launcher: cmdInput,
-                        metadata: [
-                            "description": descInput,
-                            "header_img": headOutput,
-                            "last_played": "Never",
-                            "rating": rateInput,
-                            "genre": genreInput,
-                            "developer": devInput,
-                            "publisher": pubInput,
-                            "release_date": dateFormatter.string(from: dateInput),
-                        ],
-                        icon: iconOutput,
-                        name: nameInput,
-                        platform: platInput,
-                        is_deleted: false
-                    )
+                Button(
+                    action: {
+                        let dateFormatter: DateFormatter = {
+                            let formatter = DateFormatter()
+                            formatter.dateStyle = .long
+                            return formatter
+                        }()
 
-                    games.append(newGame)
-                    games = games.sorted()
+                        let newGame: Game = .init(
+                            launcher: cmdInput,
+                            metadata: [
+                                "description": descInput,
+                                "header_img": headOutput,
+                                "last_played": "Never",
+                                "rating": rateInput,
+                                "genre": genreInput,
+                                "developer": devInput,
+                                "publisher": pubInput,
+                                "release_date": dateFormatter.string(from: dateInput),
+                            ],
+                            icon: iconOutput,
+                            name: nameInput,
+                            platform: platInput,
+                            is_deleted: false
+                        )
 
-                    let encoder = JSONEncoder()
-                    encoder.outputFormatting = .prettyPrinted
+                        games.append(newGame)
+                        games = games.sorted()
 
-                    do {
-                        let gamesJSON = try JSONEncoder().encode(games)
+                        let encoder = JSONEncoder()
+                        encoder.outputFormatting = .prettyPrinted
 
-                        if var gamesJSONString = String(data: gamesJSON, encoding: .utf8) {
-                            // Add the necessary JSON elements for the string to be recognized as type "Games" on next read
-                            gamesJSONString = "{\"games\": \(gamesJSONString)}"
-                            writeGamesToJSON(data: gamesJSONString)
+                        do {
+                            let gamesJSON = try JSONEncoder().encode(games)
+
+                            if var gamesJSONString = String(data: gamesJSON, encoding: .utf8) {
+                                // Add the necessary JSON elements for the string to be recognized as type "Games" on next read
+                                gamesJSONString = "{\"games\": \(gamesJSONString)}"
+                                writeGamesToJSON(data: gamesJSONString)
+                            }
+                        } catch {
+                            logger.write(error.localizedDescription)
                         }
-                    } catch {
-                        logger.write(error.localizedDescription)
-                    }
 
-                    dismiss()
-                },
-                label: {
-                    Text("Save Game")
+                        dismiss()
+                    },
+                    label: {
+                        Text("Save Game")
+                    }
+                )
+                .padding()
+                .frame(maxWidth: .infinity)
+
+                HStack {
+                    Spacer()
+                        .frame(maxWidth: .infinity)
+                    Spacer()
+                        .frame(maxWidth: .infinity)
+
+                    Button(
+                        action: {
+                            openURL(URL(string: "https://github.com/PhoenixLauncher/Phoenix/blob/main/setup.md")!)
+                        }, label: {
+                            ZStack {
+                                Circle()
+                                    .strokeBorder(Color(NSColor.separatorColor), lineWidth: 0.5)
+                                    .background(Circle().foregroundColor(Color(NSColor.controlColor)))
+                                    .shadow(color: Color(NSColor.separatorColor).opacity(0.3), radius: 1)
+                                    .frame(width: 20, height: 20)
+                                Text("?").font(.system(size: 15, weight: .medium))
+                            }
+                        }
+                    )
+                    .buttonStyle(PlainButtonStyle())
+                    .frame(maxWidth: .infinity)
                 }
-            )
-            .padding()
+            }
         }
-        .frame(minWidth: 768, maxWidth: 1024, maxHeight: 2000)
+        .frame(idealWidth: 800)
     }
 }
