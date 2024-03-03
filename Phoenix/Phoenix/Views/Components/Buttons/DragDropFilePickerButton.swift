fc08c99f6523fdb64df5eb1e5a77156493276966
jxhug <james@jxhug.me>
2024-03-03 14:14:08 -0800

    finalize drag and drop import button
    Signed-off-by: jxhug <james@jxhug.me>

--- Phoenix/Phoenix/Views/Components/Buttons/DragDropFilePickerButton.swift
+++ Phoenix/Phoenix/Views/Components/Buttons/DragDropFilePickerButton.swift
@@ -11,34 +11,49 @@ import UniformTypeIdentifiers
 struct DragDropFilePickerButton: View {
     @EnvironmentObject var appViewModel: AppViewModel
     
-    @Binding var launcher: String;
+    @Binding var launcher: String
     
     @State private var isImporting: Bool = false
     @State var appURL: URL?
     
     var body: some View {
-        Button(action: {
-            isImporting.toggle()
-        }, label: {
-            Text("Application goes here")
-                .foregroundColor(.gray)
-                .padding()
-        })
-        .background(Color(red: 0, green: 0, blue: 0.5))
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Game")
+                let path = (launcher.range(of: #""([^"]+)""#, options: .regularExpression) != nil) ? String(launcher[launcher.range(of: #""([^"]+)""#, options: .regularExpression)!].dropFirst().dropLast()) : ""
+                if let url = URL(string: path) {
+                    Text(("\(String(localized: "editGame_Command_SelectedGame")): \(url.path)"))
+                        .foregroundColor(.secondary)
+                        .font(.caption)
+                } else {
+                    Text(LocalizedStringKey("editGame_Command_DragDrop"))
+                        .foregroundColor(.secondary)
+                        .font(.caption)
+                }
+            }
+            Spacer()
+            Button(
+                action: {
+                    isImporting = true
+                },
+                label: {
+                    Text(LocalizedStringKey("editGame_Browse"))
+                }
+            )
+        }
+        .padding()
         .fileImporter(isPresented: $isImporting , allowedContentTypes: [.application], allowsMultipleSelection: false)
         { result in
             do {
-                let selectedAppURL: URL = try result.get().first ?? URL(fileURLWithPath: "")
-                appURL = selectedAppURL
-                if let appURL = appURL {
-                    launcher = "open \"\(appURL.absoluteString)\""
-                } else {
-                    appViewModel.failureToastText = "Unable to create application launch command"
-                    appViewModel.showFailureToast.toggle()
+                let selectedAppURL: URL? = try result.get().first
+                if let selectedAppURL = selectedAppURL {
+                    appURL = selectedAppURL
+                    launcher = "open \"\(selectedAppURL.absoluteString)\""
                 }
             }
             catch {
-                appViewModel.failureToastText = "Unable to create application launch command"
+                logger.write(error.localizedDescription)
+                appViewModel.failureToastText = "Unable to create application launch command: \(error)"
                 appViewModel.showFailureToast.toggle()
             }
        }
@@ -53,15 +68,16 @@ struct DragDropFilePickerButton: View {
             print(provider)
             // Check if the dropped item is a file URL
             provider.loadItem(forTypeIdentifier: UTType.application.identifier, options: nil) { item, error in
+                if let error = error {
+                    logger.write(error.localizedDescription)
+                    appViewModel.failureToastText = "Unable to create application launch command: \(error)"
+                    appViewModel.showFailureToast.toggle()
+                    return
+                }
                 if let url = (item as? URL) {
                     // Update the droppedURL state
-                    self.appURL = url
-                    if let appURL = appURL {
-                        launcher = "open \"\(appURL.absoluteString)\""
-                    } else {
-                        appViewModel.failureToastText = "Unable to create application launch command"
-                        appViewModel.showFailureToast.toggle()
-                    }
+                    appURL = url
+                    launcher = "open \"\(url.absoluteString)\""
                 }
             }
         }
